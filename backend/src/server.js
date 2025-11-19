23547dd3f795fdeab3d461869cc571ca25bd6b5f
vishalvishwakarma6688 <123563566+vishalvishwakarma6688@users.noreply.github.com>
2025-11-19 13:53:03 +0530

    Fix: Move redirect route below CORS

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,34 +8,39 @@ import { Link } from "./models/Link.js";
 dotenv.config();
 
 const app = express();
+
 app.use(cors({
   origin: [
     "http://localhost:3000",
     "https://tinylink-assignment-cbnpetnmg-vishals-projects-09d6ab4d.vercel.app"
   ],
   methods: ["GET", "POST", "DELETE"],
-  credentials: true,
+  allowedHeaders: ["Content-Type"],
 }));
+
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/links", linkRoutes);
 
 app.get("/healthz", (req, res) => {
   res.status(200).json({ ok: true, version: "1.0" });
 });
-
 app.get("/:code", async (req, res) => {
   try {
     const { code } = req.params;
     const link = await Link.findOne({ code });
+
     if (!link) {
       return res.status(404).json({ message: "Link not found" });
     }
+
     link.totalClicks += 1;
     link.lastClicked = new Date();
     await link.save();
+
     return res.redirect(302, link.url);
+
   } catch (err) {
     console.error("Error redirecting:", err);
     return res.status(500).json({ message: "Server error" });
